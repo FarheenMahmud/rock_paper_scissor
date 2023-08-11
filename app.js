f2c4d7c5f8ffd8f50a8e8ca62a1f81d6529ffc4c
FarheenMahmud <farheenmahmud8256@gmail.com>
2023-08-11 09:28:14 -0400

    Add functions and methods

---
+++ app.js
@@ -0,0 +1,9 @@
+function playRound(playerSelection, computerSelection){
+//your code here!
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoise();
+
+console.log(playRound(playerSelection, computerSelection));
