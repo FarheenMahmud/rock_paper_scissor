5075b414c87735767cd0b5183c25c975a3f7be27
FarheenMahmud <farheenmahmud8256@gmail.com>
2023-08-11 10:39:53 -0400

    add function

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-getComputerChoice()
+getComputerChoice(){
 
-
-
-
-function playRound(playerSelection, computerSelection){
-//your code here!
 }
+
+
+function game(){
+
+    function playRound(playerSelection, computerSelection){
+       
+
+        if(playerScore === 5 || computerScore ===5){
+            declareWinner();
+         }
+    }
+
+}
+
+
 
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection));
-
-function game(){
-
-}
