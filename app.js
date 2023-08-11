67eb0db267b7e8320ab11ba325d84146a6912349
FarheenMahmud <farheenmahmud8256@gmail.com>
2023-08-11 09:42:11 -0400

    add getComputerChoice()

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+getComputerChoice(){
+
+}
+
+
+
+
 function playRound(playerSelection, computerSelection){
 //your code here!
 }
 
 
 const playerSelection = "rock";
-const computerSelection = getComputerChoise();
+const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection));
 
 function game(){
-    
+
 }
