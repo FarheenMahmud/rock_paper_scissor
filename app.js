e0fd1dcce2b9463406851c33a7d4509047fc8845
FarheenMahmud <farheenmahmud8256@gmail.com>
2023-08-12 09:06:00 -0400

    add conditionals for playRound function

--- app.js
+++ app.js
@@ -8,8 +8,18 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
+    if(playerSelection === rock && computerSelection === paper){
+        return "You win! Paper beats Rock!"
+    }
+
+    else if(playerSelection === paper && computerSelection === rock){
+        return "You lose! Rock beats paper!"
+    }
+
+    else if(playerSelection === "Paper beats rock!")
 
   }
+
    
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
