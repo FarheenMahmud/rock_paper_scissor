9e0a50fef4250a2d8a6125be0e3ad3a3c1881822
FarheenMahmud <farheenmahmud8256@gmail.com>
2023-08-12 10:07:35 -0400

    add conditional

--- app.js
+++ app.js
@@ -8,26 +8,37 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     
-    var rps = prompt("Rock, paper, or scissors?");
+    var playerSelection = prompt("Rock, paper, or scissors?");
 
     const choices = ["rock", "paper", "scissors"];
 
+    var rps = choices.indexOf(playerSelection.toLowerCase());
+
+    if (rps > -1) 
+ //main conditional
+
+    {
+
+        if(playerSelection === "paper" && computerSelection === "rock"){
+            return "You win! Paper beats Rock!";
+        }
+    
+        else if(playerSelection === "paper" && computerSelection === "rock"){
+            return "You lose! Rock beats paper!";
+        }
+    
+        else if(playerSelection === "paper" && computerSelection === "scissors" ){
+            return "You lose! Scissors beats paper";
+        }
     
 
-
-
-    if(playerSelection === "paper" && computerSelection === "rock"){
-        return "You win! Paper beats Rock!";
     }
 
-    else if(playerSelection === "paper" && computerSelection === "rock"){
-        return "You lose! Rock beats paper!";
+    else {
+        alert ("Invalid choice!");
     }
 
-    else if(playerSelection === "paper" && computerSelection === "scissors" ){
-        return "You lose! Scissors beats paper";
-    }
-
+  
   }
 
    
